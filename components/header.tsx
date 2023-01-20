28ab8362194f3046b27714cc9a68fe29735f200f
Chad Wright <justdeejays@gmail.com>
2023-01-20 11:12:44 +0200

    feat: add initial header component

---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="w-full h-[95px] bg-blue border-b-[3px] border-b-beige drop-shadow-md">
+      <h1 className="text-beige">header</h1>
+    </header>
+  );
+};
+
+export default Header;
