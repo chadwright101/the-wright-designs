dd25b6eb6bf5254740a249a66fa423a1d5867c84
Chad Wright <justdeejays@gmail.com>
2023-01-20 11:28:32 +0200

    feat: add header logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,18 @@
-const Header = () => {
+import Image from "next/image";
+import logo from "../public/the-wright-designs-logo.png";
+
+const Header = (cssClasses?: string) => {
   return (
-    <header className="w-full h-[95px] bg-blue border-b-[3px] border-b-beige drop-shadow-md">
-      <h1 className="text-beige">header</h1>
+    <header
+      className={`w-full h-[95px] bg-blue border-b-[3px] border-b-beige drop-shadow-md py-6 px-[30px] tablet:px-[50px] tablet:py-5 ${cssClasses}`}
+    >
+      <div className="desktop:max-w-[1300px] desktop:m-auto">
+        <Image
+          src={logo}
+          alt="The Wright Designs logo"
+          className="h-[48px] w-[252.28px] tablet:w-[302px] tablet:h-[58px]"
+        />
+      </div>
     </header>
   );
 };
