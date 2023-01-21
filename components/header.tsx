26060294d1a43587f36569a3bc950cb5855305fb
Chad Wright <justdeejays@gmail.com>
2023-01-21 09:25:41 +0200

    fix: minor changes to globals.css & header prop

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,10 @@
 import Navigation from "./navigation";
 
-const Header = (cssClasses?: string) => {
+interface Props {
+  cssClasses?: string;
+}
+
+const Header = ({ cssClasses }: Props) => {
   return (
     <header
       className={`w-full min-h-[95px] bg-blue border-b-[3px] border-b-beige drop-shadow-md py-6 px-[30px] tablet:px-[50px] tablet:py-5 ${cssClasses}`}
