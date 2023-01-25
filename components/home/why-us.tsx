b5e396983fc9812c8bf8a82413f35a5f19027529
Chad Wright <justdeejays@gmail.com>
2023-01-25 15:33:00 +0200

    feat: add why us component & minor button fix

---
+++ components/home/why-us.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+
+import Button, { ButtonVariant } from "@/components/button";
+
+import whyUsList from "../../data/home/why-us-list.json";
+
+interface Props {
+  cssClasses?: string;
+}
+
+const WhyUs = ({ cssClasses }: Props) => {
+  return (
+    <section className={`${cssClasses}`}>
+      <h2 className="text-heading text-center mb-10">Why us?</h2>
+      <ul className="flex flex-wrap gap-12 justify-center tablet:justify-around tablet:gap-8">
+        {whyUsList.map(({ heading, paragraph, image }, index) => (
+          <li className="max-w-[550px] tablet:max-w-[330px]" key={index}>
+            <ul className="flex flex-col gap-4 items-center text-center">
+              <li className="flex flex-col items-center gap-4">
+                <Image src={image.src} alt={image.alt} width={46} height={46} />
+                <h3 className="text-subheading">{heading}</h3>
+              </li>
+              <li>
+                <p>{paragraph.first}</p>
+              </li>
+              <li>
+                <p>{paragraph.second}</p>
+              </li>
+              <li>
+                <p>{paragraph.third}</p>
+              </li>
+            </ul>
+          </li>
+        ))}
+      </ul>
+      <Button url="/portfolio" cssClasses="mx-auto mt-14 tablet:mt-10">
+        View our portfolio
+      </Button>
+    </section>
+  );
+};
+
+export default WhyUs;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,11 @@
+import WhyUs from "@/components/home/why-us";
 import HeroComponent from "../components/home/hero-component";
 
 export default function Home() {
   return (
     <>
       <HeroComponent />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
-        eum assumenda veritatis voluptate deleniti doloribus, animi omnis velit,
-        ducimus natus nihil doloremque ex debitis dolores repellendus quos fuga.
-        Rerum quaerat dolorum odio, dignissimos debitis in dolor error cum? Quo
-        nam, optio provident accusamus hic culpa inventore quis vero. At
-        inventore et natus alias. Provident, quos fugit magnam officiis commodi
-        facere sit iusto perspiciatis aut maxime quo dignissimos natus
-        reiciendis dolorum iste maiores id sapiente exercitationem eaque. Nam
-        quas accusamus rerum voluptates dolore natus cum at delectus veniam
-        facilis totam magni possimus et sapiente similique dolorum itaque quidem
-        quae, recusandae autem! Et magnam quia animi unde reiciendis culpa
-        nesciunt repudiandae modi aperiam praesentium tempore, incidunt quasi
-        pariatur iste! Tempore aliquid quia iure, deleniti molestiae quis
-        repellat doloremque alias iusto vero vel sequi ducimus neque officiis id
-        quaerat asperiores necessitatibus animi eius pariatur ut debitis
-        mollitia. Aliquam sed explicabo vero quis animi impedit hic mollitia
-        nihil rerum quae sequi molestias saepe assumenda natus iure porro,
-        voluptatibus aspernatur vitae fuga! Doloribus repudiandae atque harum
-        libero quas ut quidem, sapiente minima sunt aspernatur architecto ea eum
-        dolores laboriosam obcaecati blanditiis quibusdam accusantium, a
-        consectetur fugit, hic nisi maiores reprehenderit explicabo. Saepe,
-        corrupti excepturi tenetur commodi esse voluptates officia sunt laborum.
-        Nulla tempora aliquid qui sed amet reiciendis quam tempore neque
-        expedita, fugit provident eligendi fugiat dolores iste velit cumque
-        accusamus maiores nobis doloribus recusandae ipsam illo vero! Inventore
-        minus harum quidem saepe temporibus, dolores veniam ipsum. Expedita,
-        totam distinctio. Illo sint dolor voluptas nisi nihil. Recusandae
-        veniam, asperiores in sapiente quaerat nostrum voluptatem eligendi!
-        Molestiae, inventore! Odit eaque aperiam saepe, aspernatur inventore
-        placeat harum ex obcaecati sapiente deleniti repudiandae, eveniet hic
-        animi ab? Quas ut tempora minima similique, minus vitae voluptatibus
-        assumenda totam quod nostrum, libero eos suscipit ea aperiam pariatur
-        officiis necessitatibus provident saepe. Laudantium quidem quasi odit id
-        culpa fuga, rerum quae, sit reiciendis, exercitationem similique ut
-        deleniti expedita. Cumque, est voluptatibus. Voluptatem id provident
-        repudiandae dicta a. Est nostrum recusandae et labore ut facere nemo,
-        sed, quisquam dignissimos doloremque consequatur laudantium, deleniti
-        explicabo vero unde. Et magni aliquam vel dolores expedita in iste hic,
-        odit quibusdam itaque incidunt odio? Ab beatae sunt, cumque, hic
-        doloribus deserunt eum porro quam possimus molestias quaerat ad tenetur!
-        Reiciendis laudantium iste dignissimos quia necessitatibus fuga enim
-        odit architecto in minus aperiam, dicta aliquam explicabo mollitia ea
-        voluptate dolor! Consectetur quis nobis temporibus quam labore itaque
-        impedit tempora, veniam voluptatibus fuga corporis vel, laudantium
-        aspernatur blanditiis obcaecati numquam, ipsam tempore repellat et totam
-        quibusdam? Reiciendis alias assumenda porro autem nobis. Consectetur
-        quos placeat similique sint aut laudantium accusamus, vel deleniti,
-        architecto nam, reiciendis omnis assumenda nesciunt sequi doloremque ab
-        soluta eius animi? Architecto quas illum, amet ipsa, unde quos explicabo
-        adipisci commodi in dolor iusto cumque cum? Illum odio, aperiam
-        blanditiis eos non, dicta enim accusantium optio explicabo ad eligendi
-        impedit earum labore similique facere fuga! Natus laboriosam modi porro
-        qui ullam voluptate deserunt commodi asperiores itaque ea corporis,
-        totam cum iste sunt, perferendis accusantium quisquam.
-      </p>
+      <WhyUs />
     </>
   );
 }
