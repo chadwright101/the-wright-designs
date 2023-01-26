c4b4e0c1bc7f5c008b85838242c0bc3cd9b9aa9f
Chad Wright <justdeejays@gmail.com>
2023-01-26 11:28:58 +0200

    feat: add about component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import About from "@/components/home/about";
 import WhyUs from "@/components/home/why-us";
 import HeroComponent from "../components/home/hero-component";
 
@@ -5,7 +6,8 @@ export default function Home() {
   return (
     <>
       <HeroComponent />
-      <WhyUs />
+      <WhyUs cssClasses="my-20 tablet:my-16" />
+      <About />
     </>
   );
 }
