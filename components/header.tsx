1bb94a4a970643f17f231e8d1a686c914e091d38
Chad Wright <justdeejays@gmail.com>
2023-02-01 14:06:42 +0200

    fix: adjust swipe in animation portfolio component

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,7 @@ interface Props {
 const Header = ({ cssClasses }: Props) => {
   return (
     <header
-      className={`w-full min-h-[95px] bg-blue border-b-[3px] border-b-beige drop-shadow-md py-6 px-[30px] tablet:px-[50px] tablet:py-5 sticky top-0 z-10 ${cssClasses}`}
+      className={`w-full min-h-[95px] bg-blue border-b-[3px] border-b-beige drop-shadow-md py-6 px-[30px] tablet:px-[50px] tablet:py-5 sticky top-0 z-20 ${cssClasses}`}
     >
       <Navigation />
     </header>
