a2f7751223ff6fa50e10abed210b2a53e4376441
Chad Wright <justdeejays@gmail.com>
2023-02-02 07:52:30 +0200

    feat: add odd slide animation direction change

--- components/home/why-us.tsx
+++ components/home/why-us.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Button from "@/components/button";
 import FadeIn from "@/components/fade-in";
 import SwipeLeftToRight from "../swipe-left-right";
+import SwipeRightToLeft from "../swipe-right-left";
 
 import whyUsList from "../../data/home/why-us-list.json";
 
@@ -15,34 +16,73 @@ const WhyUs = ({ cssClasses }: Props) => {
     <section className={`${cssClasses}`}>
       <h2 className="text-heading text-center mb-10">Why us?</h2>
       <ul className="flex flex-wrap gap-12 justify-center tablet:justify-around tablet:gap-8">
-        {whyUsList.map(({ heading, paragraph, image, delay }, index) => (
-          <SwipeLeftToRight delay={delay} key={index}>
-            <li className="max-w-[550px] tablet:max-w-[330px]">
-              <ul className="flex flex-col gap-4 items-center text-center">
-                <li key={index} className="flex flex-col items-center gap-4">
-                  <FadeIn delay={delay} bounce>
-                    <Image
-                      src={image.src}
-                      alt={image.alt}
-                      width={46}
-                      height={46}
-                    />
-                  </FadeIn>
-                  <h3 className="text-subheading">{heading}</h3>
-                </li>
-                <li>
-                  <p>{paragraph.first}</p>
-                </li>
-                <li>
-                  <p>{paragraph.second}</p>
-                </li>
-                <li>
-                  <p>{paragraph.third}</p>
-                </li>
-              </ul>
-            </li>
-          </SwipeLeftToRight>
-        ))}
+        {whyUsList.map(
+          ({ heading, paragraph, image, delay, fromRight }, index) => (
+            <>
+              {fromRight ? (
+                <SwipeLeftToRight delay={delay} key={index}>
+                  <li className="max-w-[550px] tablet:max-w-[330px]">
+                    <ul className="flex flex-col gap-4 items-center text-center">
+                      <li
+                        key={index}
+                        className="flex flex-col items-center gap-4"
+                      >
+                        <FadeIn delay={delay} bounce>
+                          <Image
+                            src={image.src}
+                            alt={image.alt}
+                            width={46}
+                            height={46}
+                          />
+                        </FadeIn>
+                        <h3 className="text-subheading">{heading}</h3>
+                      </li>
+                      <li>
+                        <p>{paragraph.first}</p>
+                      </li>
+                      <li>
+                        <p>{paragraph.second}</p>
+                      </li>
+                      <li>
+                        <p>{paragraph.third}</p>
+                      </li>
+                    </ul>
+                  </li>
+                </SwipeLeftToRight>
+              ) : (
+                <SwipeRightToLeft delay={delay} key={index}>
+                  <li className="max-w-[550px] tablet:max-w-[330px]">
+                    <ul className="flex flex-col gap-4 items-center text-center">
+                      <li
+                        key={index}
+                        className="flex flex-col items-center gap-4"
+                      >
+                        <FadeIn delay={delay} bounce>
+                          <Image
+                            src={image.src}
+                            alt={image.alt}
+                            width={46}
+                            height={46}
+                          />
+                        </FadeIn>
+                        <h3 className="text-subheading">{heading}</h3>
+                      </li>
+                      <li>
+                        <p>{paragraph.first}</p>
+                      </li>
+                      <li>
+                        <p>{paragraph.second}</p>
+                      </li>
+                      <li>
+                        <p>{paragraph.third}</p>
+                      </li>
+                    </ul>
+                  </li>
+                </SwipeRightToLeft>
+              )}
+            </>
+          )
+        )}
       </ul>
       <Button
         url="/portfolio"
