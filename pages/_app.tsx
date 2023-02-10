3f9a1acba3e15493a517b25b770c31fc297a0849
Chad Wright <justdeejays@gmail.com>
2023-02-10 10:40:45 +0200

    fix: remove google scripts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,12 @@
 import type { AppProps } from "next/app";
-import Script from "next/script";
-
 import Layout from "@/components/layout";
 
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM_MCVZ25T');
-      `}
-      </Script>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
