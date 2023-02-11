8484129fcc3b6cdb3d5192494218df5fe5124858
Chad Wright <justdeejays@gmail.com>
2023-02-11 09:03:18 +0200

    fix: add escapes to about section

--- components/home/about.tsx
+++ components/home/about.tsx
@@ -16,19 +16,20 @@ const About = ({ cssClasses }: Props) => {
         <p>
           We specialise in providing quality & modern tailor-made solutions
           through eye-catching designs, which are then implemented through clean
-          and functional development. If you're feeling the need to break free
-          of a theme or template that's a bit too restrictive, outdated or hard
-          to manage, we just might have the answer for you.
+          and functional development. If you&apos;re feeling the need to break
+          free of a theme or template that&apos;s a bit too restrictive,
+          outdated or hard to manage, we just might have the answer for you.
         </p>
         <p>
-          We're all about fundamentals, which is why our communication based
-          approach allows for you, our client, to be as hands on, or hands off
-          as you'd like. If you're looking to be included in each step of the
-          process to ensure the final product is exactly what you want, we're
-          game. Or perhaps you've got a lot on your plate and need a trusted and
-          experienced one stop shop to getting online, that&apos;s no problem
-          either. Our team is flexible and willing to adapt to whatever is
-          needed to get the job done correctly by deadline day.
+          We&apos;re all about fundamentals, which is why our communication
+          based approach allows for you, our client, to be as hands on, or hands
+          off as you&apos;d like. If you&apos;re looking to be included in each
+          step of the process to ensure the final product is exactly what you
+          want, we&apos;re game. Or perhaps you&apos;ve got a lot on your plate
+          and need a trusted and experienced one stop shop to getting online,
+          that&apos;s no problem either. Our team is flexible and willing to
+          adapt to whatever is needed to get the job done correctly by deadline
+          day.
         </p>
         <p>
           Our guarantee is and has always been a hassle-free design &
