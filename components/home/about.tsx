452b89d54b7dce8e139a6469ce5064887ecee8ce
Chad Wright <justdeejays@gmail.com>
2023-02-11 08:59:47 +0200

    fix: adjust copy on about, services and why us

--- components/home/about.tsx
+++ components/home/about.tsx
@@ -8,20 +8,32 @@ const About = ({ cssClasses }: Props) => {
       <h2 className="text-heading text-center mb-10">About</h2>
       <div className="text-center flex flex-col gap-4 max-w-[550px] mx-auto tablet:max-w-none tablet:mx-0">
         <p>
-          The Wright Designs is a small, community orientated business, aimed at
-          helping other small to medium businesses grow. We specialise in
-          providing modern web design, stylish e-brochure, logo and business
-          card designs, as well as professional photography.
+          The Wright Designs is comprised of a small team opporating from
+          Plettenberg Bay, South Africa. Our aim is to assist other small to
+          medium businesses grow their online precence, which starts with a
+          strong website.
         </p>
         <p>
-          Our services are tailor-made. This allows our clients to be a part of
-          the design process as much as they would like to and makes it possible
-          for us to quote according to our clients&apos; particular needs-
-          allowing them a personalised package that suits their pocket.
+          We specialise in providing quality & modern tailor-made solutions
+          through eye-catching designs, which are then implemented through clean
+          and functional development. If you're feeling the need to break free
+          of a theme or template that's a bit too restrictive, outdated or hard
+          to manage, we just might have the answer for you.
         </p>
         <p>
-          We pride ourselves on our hands-on approach and personal touch. We
-          guarantee a hassle-free process and professional after-sales service.
+          We're all about fundamentals, which is why our communication based
+          approach allows for you, our client, to be as hands on, or hands off
+          as you'd like. If you're looking to be included in each step of the
+          process to ensure the final product is exactly what you want, we're
+          game. Or perhaps you've got a lot on your plate and need a trusted and
+          experienced one stop shop to getting online, that&apos;s no problem
+          either. Our team is flexible and willing to adapt to whatever is
+          needed to get the job done correctly by deadline day.
+        </p>
+        <p>
+          Our guarantee is and has always been a hassle-free design &
+          development process, followed by a professional & efficient
+          after-sales service.
         </p>
       </div>
     </section>
