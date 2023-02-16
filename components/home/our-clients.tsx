a88e85a80c5bd6dfb8473c72fd937b6c9ee8ea46
Chad Wright <justdeejays@gmail.com>
2023-02-16 10:38:50 +0200

    feat: add button to our clients component

--- components/home/our-clients.tsx
+++ components/home/our-clients.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import Button from "../button";
+
 import ourClients from "../../data/home/our-clients.json";
 
 import { Splide, SplideSlide } from "@splidejs/react-splide";
@@ -14,46 +16,51 @@ const OurClients = ({ cssClasses }: Props) => {
   return (
     <section className={`${cssClasses}`}>
       <h2 className="text-heading text-center mb-10">Our Clients</h2>
-      <Splide
-        options={{
-          autoplay: true,
-          type: "loop",
-          interval: 2500,
-          speed: 1500,
-          perPage: 3,
-          drag: false,
-          pagination: false,
-          arrows: false,
-          lazyLoad: "nearby",
-          breakpoints: {
-            425: {
-              perPage: 1,
+      <div className="flex flex-col items-center gap-16">
+        <Splide
+          options={{
+            autoplay: true,
+            type: "loop",
+            interval: 2500,
+            speed: 1500,
+            perPage: 3,
+            drag: false,
+            pagination: false,
+            arrows: false,
+            lazyLoad: "nearby",
+            breakpoints: {
+              425: {
+                perPage: 1,
+              },
+              725: {
+                perPage: 2,
+                gap: "1.5rem",
+              },
+              900: {
+                gap: "1.25rem",
+              },
             },
-            725: {
-              perPage: 2,
-              gap: "1.5rem",
-            },
-            900: {
-              gap: "1.25rem",
-            },
-          },
-        }}
-        className="mt-8"
-      >
-        {ourClients.map(({ src, alt, url }, index) => (
-          <SplideSlide key={index} className="grid place-items-center">
-            <Link href={url} target="_blank">
-              <Image
-                src={src}
-                alt={alt}
-                width={350}
-                height={350}
-                className="drop-shadow-md w-[300px] phoneSmall:w-[200px] h-auto phoneLarge:w-[250px] tablet2:w-[300px] desktop:w-[350px]"
-              />
-            </Link>
-          </SplideSlide>
-        ))}
-      </Splide>
+          }}
+          className="mt-8"
+        >
+          {ourClients.map(({ src, alt, url }, index) => (
+            <SplideSlide key={index} className="grid place-items-center">
+              <Link href={url} target="_blank">
+                <Image
+                  src={src}
+                  alt={alt}
+                  width={350}
+                  height={350}
+                  className="drop-shadow-md w-[300px] phoneSmall:w-[200px] h-auto phoneLarge:w-[250px] tablet2:w-[300px] desktop:w-[350px]"
+                />
+              </Link>
+            </SplideSlide>
+          ))}
+        </Splide>
+        <Button url="/portfolio" blueBackground beigeArrows beigeText>
+          View more
+        </Button>
+      </div>
     </section>
   );
 };
