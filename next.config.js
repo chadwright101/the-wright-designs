5bca11da89c9c0a23879a643b555e3c9d5ebb5bb
Chad Wright <justdeejays@gmail.com>
2024-09-24 18:03:49 +0200

    feat: add small desktop screenshot assets

--- next.config.js
+++ next.config.js
@@ -4,15 +4,6 @@ const nextConfig = {
   images: {
     deviceSizes: [425, 800, 1400],
     imageSizes: [425, 800, 1400],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname:
-          "the-wright-designs-website-images.s3.af-south-1.amazonaws.com",
-        port: "",
-        pathname: "/*/**",
-      },
-    ],
   },
 };
 
