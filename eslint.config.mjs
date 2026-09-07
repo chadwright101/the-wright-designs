406ede923f8e0e6e92d2b3dcb7761713a16cc14f
Chad Wright <justdeejays@gmail.com>
2026-09-07 09:50:09 +0200

    feat: add bay holiday homes to portfolio

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+
+const eslintConfig = [
+  ...nextCoreWebVitals,
+  {
+    ignores: [".next/**", "node_modules/**", "out/**", "build/**"],
+  },
+];
+
+export default eslintConfig;
